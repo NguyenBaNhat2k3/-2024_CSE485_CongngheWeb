2ce1bb66143655ceac870650728823c5b8e1cb24
Hung Huy Vu <119580184+vuhuyhung17082003@users.noreply.github.com>
2024-03-01 21:31:26 +0700

    Add files via upload

---
+++ lab2/project24/user/php/login.php
@@ -0,0 +1,22 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login</title>
+</head>
+<body>
+<form action="process_login.php" method="post">
+    <h2><b>LOGIN</b></h2> <br>
+    Username: <input type="text" name="username" required> <br>
+    Password: <input type="password" name="password" required> <br>
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
